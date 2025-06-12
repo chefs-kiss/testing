e76c722a3aebc6eb7a2b30140b523e3784786c3e
Kim Mandery <mander1@stolaf.edu>
2025-06-12 16:27:41 +0000

    fib numb

--- workshop.py
+++ workshop.py
@@ -13,3 +13,18 @@ print(b)
 for _ in range(10):
   print(a+b)
   a,b = b,a+b
+
+#fib func
+def fibonacci(n):
+    if n == 0:
+        return 0
+    elif n == 1:
+        return 1
+
+    else:
+        return fibonacci(n - 1) + fibonacci(n - 2)
+
+print(f"Here is the 10th Fibonacci number",fibonacci(10))
+
+
+
